e3466a85c22b443364ced0ea00a2183e34997601
Nathaniel Watson <nathankw@stanford.edu>
2016-11-10 00:22:31 -0800

    Updated how projects are searched for.

--- dnanexus_utils.py
+++ dnanexus_utils.py
@@ -97,7 +97,7 @@ class DxSeqResults:
 	DX_FASTQC_FOLDER = "/stage1_qc/fastqc_reports"
 	DX_QC_REPORT_FOLDER = "/stage2_qc_report"
 
-	def __init__(self,dx_username,dx_project_id=False,dx_project_name=False,library_name=False,billing_account_id=None):
+	def __init__(self,dx_username,dx_project_id=False,dx_project_name=False,uhts_run_name=False,sequencing_lane=False,library_name=False,billing_account_id=None):
 		"""
 		Description : Logs the specified user into DNAnexus, and then finds the DNAnexus sequencing results project that was uploaded by 
 									SCGPM. The project can be precisely retrieved if the projecd ID is specified (via the dx_project_id argument).
@@ -112,7 +112,11 @@ class DxSeqResults:
 	  Args : dx_username - str. The login name of a DNAnexus user.
 		Args : dx_project_id - str. The ID of the DNAnexus project. If specified, no project search will be performed as it will be
 						directly retrieved.
-					 dx_project_name - the name of a DNAnexus project containing sequencing results that were uploaded by SCGPM. 
+					 dx_project_name - The name of a DNAnexus project containing sequencing results that were uploaded by SCGPM. 
+					 uhts_run_name - The name of the sequencing run in UHTS. This is added as a property to all projects in DNAnexus through 
+													 the 'seq_run_name' property.
+					 sequencing_lane - The lane number of the flowcell on which the library was sequenced. This is added as a property to all 
+						                 projects in DNAnexus through the 'seq_lane_index' property.
 					 library_name - str. The library name of the sample that was sequenced. This is name of the library that was 
 														submitted to SCGPM for sequencing. This is added as a property to all sequencing result projects through
 														the 'library_name' property.
@@ -133,35 +137,57 @@ class DxSeqResults:
 		scgpm_seqresults_dnanexus.gbsc_dnanexus.utils.log_into_dnanexus(self.dx_username)
 		self.dx_project_id = dx_project_id
 		self.dx_project_name = dx_project_name
+		self.uhts_run_name = uhts_run_name
+		self.sequencing_lane = sequencing_lane
 		self.library_name = library_name
-		if not self.dx_project_id and not self.dx_project_name and not self.library_name:
-			raise Exception("One of the arguments 'dx_project_id', 'dx_project_name' or 'library_name' must be specified.")
+		if not self.dx_project_id and not self.dx_project_name and not self.uhts_run_name and not self.library_name and not self.sequencing_lane:
+			raise Exception("You must specify 'dx_project_id', or some combination of 'dx_project_name', 'uhts_run_name', 'library_name', or 'sequencing_lane.")
 		self._set_dxproject_id()
 		#_set_dxproject_id sets the following instance attributes:
 		# self.dx_project_id
 		# self.dx_project_name
 		# self.library_name
-		self._set_sequencing_run_name() #sets self.sequencing_run_name.
-		self._set_sequencing_platform() #sets self.sequencing_platform
+		if self.dx_project_id:
+			self._set_sequencing_run_name() #sets self.sequencing_run_name.
+			self._set_sequencing_platform() #sets self.sequencing_platform
 
 	def _set_dxproject_id(self):
 		"""
-		Function : Retrieves the ID of the project in DNAnexus that has the sequencing results for the library specified by
-							 self.library_name. It does this by finding the project whose property named 'library_name' is set to
-							 self.library_name.  May return None if there isn't yet a project found in DNAnexus yet, which indicates that the
-							 sequencing likely hasn't finished yet.
+		Function : Searches for the project in DNAnexus based on the input arguments when instantiating the class. If multiple projects are
+							 found based on the search criteria, an exception will be raised. A few various search strategies are employed, based on
+							 the input arguments. In all cases, if the 'billing_account_id' was specifed, all searches will search for projects only 
+							 belonging to the specified billing account. The search strategies work as follows:
+							 If the project ID was
+               provided, the search will attempt to find the project by ID only. If the project ID wasn't provided, but the project 
+							 name was specified, then the search will attempt to find the project by name and by any project properties that may have 
+							 been set during instantiation (uhts_run_name, sequencing_lane, and library_name).
+							 If neither the project name nor the project ID was specified, then a search by whichever project properties were specified
+						   will take place.
+
+							 This method will not set the self.dx_project_id if none of the search methods are successful in finding a single project,
+							 and this may indicate that the sequencing hasn't finished yet.
+
 		Returns  : str. The DNAnexus project ID or the empty string if a project wasn't found.
 		Raises   : scgpm_seqresults_dnanexus.dnanexus_utils.DxMultipleProjectsWithSameLibraryName() if the search is by self.library_name, and multiple DNAnexus projects have that library name.
 		"""
+		dx_project_props = {}
+		if self.library_name:
+			dx_project_props["library_name"] = self.library_name
+		if self.uhts_run_name:
+			dx_project_props["seq_run_name"] = self.uhts_run_name
+		if self.sequencing_lane:
+			dx_project_props["seq_lane_index"] = self.sequencing_lane
+
 		dx_proj = ""
 		if self.dx_project_id:
 			dx_proj = dxpy.DXProject(dxid=self.dx_project_id)
 		elif self.dx_project_name:
-			res = dxpy.find_one_project(billed_to=self.billing_account_id,zero_ok=True,more_ok=True,name=self.dx_project_name)
+			res = dxpy.find_one_project(properties=dx_project_props,billed_to=self.billing_account_id,zero_ok=True,more_ok=False,name=self.dx_project_name)
 			if res:
 				dx_proj = dxpy.DXProject(dxid=res["id"])
 		else:
-			res = dxpy.find_projects(billed_to=self.billing_account_id,zero_ok=True,properties={"library_name":self.library_name})
+			#try to find by library_name and potential uhts_run_name
+			res = list(dxpy.find_projects(properties=dx_project_props,billed_to=self.billing_account_id))
 			if len(res) > 1:
 				projects = [x.id for x in res]
 				raise DxMultipleProjectsWithSameLibraryName("Error - Multiple DNAnexus projects have the same value for the library_name property value of {library_name}. The projects are {projects}.".format(library_name=self.library_name,projects=projects))
@@ -398,6 +424,8 @@ class DxSeqResults:
 			barcode = props["barcode"]
 			name = f.name
 			filedest = os.path.abspath(os.path.join(dest_dir,name))
+#			import pdb
+#			pdb.set_trace()
 			if os.path.exists(filedest):
 				if overwrite:
 					print("Downloading FASTQ file {name} from DNAnexus project {project} to {path}.".format(name=f.name,project=self.dx_project_name,path=filedest))
--- scripts/download_fastqs.py
+++ scripts/download_fastqs.py
@@ -39,18 +39,34 @@ description = ""
 parser = ArgumentParser(description=description)
 
 parser.add_argument('-u',"--user-name",required=True,help="The login name of the DNAnexus user, who has at least VIEW access to the DNAnexus project containing the FASTQs of interest. An API token must have already been generated for this user and that token must have been added to the DNAnexus login configuration file located at {DX_LOGIN_CONF}.".format(DX_LOGIN_CONF=DX_LOGIN_CONF))
-parser.add_argument('-l',"--library-name",required=True,help="The library name of the sample that was sequenced. This is name of the library that was submitted to SCGPM for sequencing. This is added as a property to all sequencing result projects through the 'library_name' property.")
+parser.add_argument('-l',"--library-name",required=True,help="The library name of the sample that was sequenced. This is name of the library that was submitted to SCGPM for sequencing. This is added as a property to all sequencing result projects through the 'library_name' project property.")
+parser.add_argument("--uhts-run-name",help="The name of the sequencing run in UHTS. This is added as a property to all projects in DNAnexus through the 'seq_run_name' project property. Use this option in combination with --library-name and --lane to further restrict the search space, which is useful especially since multiple DNAnexus projects can have the same library_name property value (i.e. if resequencing the same library).") 
+parser.add_argument("--lane",type=int,help="The lane number of the flowcell on which the library was sequenced. This is added as a property to all projects in DNAnexus through the 'seq_lane_index' property. Use this in conjunction with --library-name and --uhts-run-name to further restrict the project search space.")
 parser.add_argument('-b',"--barcode",help="The barcode of the sequenced sample. If specified, then only FASTQs with this barcode will be downloaded. Otherwise, all FASTQs will be downloaded.")
 parser.add_argument("-d","--file-download-dir",required=True,help="Local directory in which to download the FASTQ files.")
+parser.add_argument("--not-found-error",action="store_true",help="Presence of this options means to raise an Exception if a project can't be found on DNAnexus with the provided input.")
 
 args = parser.parse_args()
 dx_username = args.user_name
 library_name = args.library_name
+uhts_run_name = args.uhts_run_name
+lane = args.lane
 barcode = args.barcode
 file_download_dir = args.file_download_dir
 if not os.path.exists(file_download_dir):
 	os.makedirs(file_download_dir)
+not_found_error = args.not_found_error
 
-dxsr = scgpm_seqresults_dnanexus.dnanexus_utils.DxSeqResults(dx_username=dx_username,library_name=library_name)
-fastq_dico = dxsr.download_fastqs(barcode=barcode,dest_dir=file_download_dir)
+dxsr = scgpm_seqresults_dnanexus.dnanexus_utils.DxSeqResults(dx_username=dx_username,library_name=library_name,uhts_run_name=uhts_run_name,sequencing_lane=lane)
+if dxsr.dx_project_id:
+	fastq_dico = dxsr.download_fastqs(barcode=barcode,dest_dir=file_download_dir)
+else:
+	if not_found_error:
+		msg = "Could not find DNAnexus project with library name {library_name}".format(library_name=library_name)
+		if barcode:
+			msg += " and barcode {barcode}.".format(barcode=barcode)
+		else:
+			msg += "."
+		raise Exception(msg)
+		
 #fastq_dico: Keys are the file names of the FASTQs and values are the fully qualified paths to the FASTQs.
