7ac92c3c7672fa7ebb8637f1a77a8cbb434556ea
Nathaniel Watson <nathankw@stanford.edu>
2017-03-21 14:33:01 -0700

    Added more documentation.

--- scripts/add_r1-r2_fastq_paths.py
+++ scripts/add_r1-r2_fastq_paths.py
@@ -5,7 +5,7 @@
 #2016-11-17
 ###
 
-from argparse import ArgumentParser
+import argparse
 import os
 import sys
 
@@ -14,10 +14,21 @@ import gbsc_dnanexus
 
 DX_LOGIN_CONF = gbsc_dnanexus.CONF_FILE
 
-description = "Given a tab-delimited input file with columns run name, lane, and barcode"
+description = """Given a tab-delimited input file with columns
+	1) uhts run name,
+	2) lane, and
+	3) barcode, 
 
-parser = ArgumentParser(description=description)
-parser.add_argument("-i","--infile",required=True,help="Tab-delimited input file with columns uhts run name, lane, and barcode. Empty lines and lines beginning with '#' are ignored. A field-header line starting with '#' is required as the first line.")
+	creates an output file containing the columns
+	1) FASTQ file name,
+	2) UHTS run name,
+	3) lane, and 
+	4) barcode
+There is one line per FASTQ file.
+"""
+
+parser = argparse.ArgumentParser(description=description,formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("-i","--infile",required=True,help="Tab-delimited input file with columns uhts_run name, lane, and barcode. Empty lines and lines beginning with '#' are ignored. A field-header line starting with '#' is required as the first line.")
 parser.add_argument("--fieldindex-uhts-run-name",required=True,type=int,help="The 0-base index of the uhts run name field in the input file.")
 parser.add_argument("--fieldindex-lane",required=True,type=int,help="The 0-base index of the lane field in the input file.")
 parser.add_argument("--fieldindex-barcode",required=True,type=int,help="The 0-base index of the barcode field in the input file.")
@@ -69,8 +80,13 @@ for i in sorted(dico):
 	dxsr = scgpm_seqresults_dnanexus.dnanexus_utils.DxSeqResults(dx_username=dx_username,uhts_run_name=run_name,sequencing_lane=lane)
 	if not dxsr.dx_project_id:
 		#no project found.
+		print("Warning: No DNAnexus project found for line {} in input file.".format(index + 1))
 		continue
 	fastq_files_props = dxsr.get_fastq_files_props(barcode=barcode)
+	if not fastq_files_props:
+		print("Warning: No FASTQ files found for line {} in input file.".format(index + 1))
+	if len(fastq_files_props) == 1:
+		print("Warning: There was only one FASTQ file found for line {} in input file. If this is not paired-end sequencing, you can ignore this warning.".format(index + 1))
 	for prop in fastq_files_props: 
 		#i is the DNAnexus object ID of the FASTQ file.
 		fastq_props = fastq_files_props[prop]
@@ -78,16 +94,21 @@ for i in sorted(dico):
 		file_name = fastq_props["fastq_file_name"]
 		if file_name in fastq_dir_contents:
 			dico[i]["fastq_files"][read_num] = file_name
+		else:
+			print("Warning: For line {line_num} of input file, the FASTQ file {file_name} in DNAnexus project {dx_project} does not exist in --fastq-dir={fastqdir}.".format(line_num=index + 1,file_name=file_name,dx_project=dxsr.dx_project_id, fastqdir=fastq_dir))
 
 for i in sorted(dico):
 	data = dico[i]
 	line = dico[i]["line"]
 	if data["fastq_files"]:
-		for j in sorted(data["fastq_files"]):
-			file_name = data["fastq_files"][j]
+		for read_num in sorted(data["fastq_files"]):
+			file_name = data["fastq_files"][read_num]
 			fout.write("\t".join([file_name,str(read_num),line]) + "\n")
 	else:
 		fout.write("\t\t" + line + "\n")
+	fout.flush()
+	os.fsync(fout.fileno())
+		
 fout.close()
 	
 
