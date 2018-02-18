fae76f87932f330f5968b61db3c349022a8d47b3
Nathaniel Watson <nathankw@encyclia.local>
2018-02-17 20:54:15 -0800

    Added submodules back

--- setup.py
+++ setup.py
@@ -14,13 +14,24 @@ scripts = glob.glob("scgpm_seqresults_dnanexus/scripts/*.py")
 #Remove __init__.py
 [scripts.remove(x) for x in scripts if x.endswith("__init__.py")] 
 
+packages = find_packages()
+gbsc_dnanexus_packages = find_packages(where="gbsc_dnanexus")
+scgpm_lims_packages = find_packages(where="scgpm_lims")
+packages.extend(gbsc_dnanexus_packages)
+packages.extend(scgpm_lims_packages)
+
 setup(
   name = "scgpm_seqresults_dnanexus",
   version = "0.1.0",
+  license = "MIT",
   description = "Utilities for working with the SCGPM Sequencing Center application logic on DNAnexus.",
   author = "Nathaniel Watson",
   author_email = "nathankw@stanford.edu",
   url = "https://github.com/StanfordBioinformatics/scgpm_seqresults_dnanexus.git",
-  packages = find_packages(),
+  packages = packages,
+  package_dir = {
+    "scgpm_lims": "scgpm_lims/scgpm_lims",
+    "gbsc_dnanexus": "gbsc_dnanexus/gbsc_dnanexus"
+  },
   scripts = scripts
 )
